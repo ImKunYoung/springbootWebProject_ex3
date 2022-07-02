2c96a0f10aa2dad94304075c0b5cb3f18b501f7e
ImKunYoung <lim8667@gmail.com>
2022-07-03 04:09:34 +0900

    세팅

---
+++ src/main/java/com/example/springbootwebproject_ex3/SpringbootWebProjectEx3Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebproject_ex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebProjectEx3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebProjectEx3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootwebproject_ex3/SpringbootWebProjectEx3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebproject_ex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebProjectEx3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
